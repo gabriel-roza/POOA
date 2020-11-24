d74dc5910acc3cbbd57728aa35fc2c4d2b35ddf5
gabriel-roza <54413295+gabriel-roza@users.noreply.github.com>
2020-11-24 10:54:22 -0300

    Create pru.java

---
+++ pru.java
@@ -0,0 +1,14 @@
+public class Cliente
+{
+    public int ClienteId {...}
+    public string Nome {...}
+    public string Email {...}
+    public string CPF {...}
+ 
+    public double CalcularDívida(Dívida pagar){
+        if (pagar.Valor > 0){
+            return pagar.Valor
+        }
+        return 0;
+    }
+}
